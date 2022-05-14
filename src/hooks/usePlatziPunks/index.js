70c4817452b1d8a7b41995928b1e37808915f14a
Juan Fernandez <jsebas1198@protonmail.com>
2022-05-14 16:10:58 -0400

    dissable eslint-next-line for index.js on usePlatziPunks folder

--- src/hooks/usePlatziPunks/index.js
+++ src/hooks/usePlatziPunks/index.js
@@ -2,22 +2,22 @@ import { useMemo } from "react";
 import { useWeb3React } from "@web3-react/core";
 import PlatziPunksArtifact from "../../config/web3/artifacts/PlatziPunks";
 
-const {address,abi} = PlatziPunksArtifact;
+const { address, abi } = PlatziPunksArtifact;
 
 const usePlatziPunks = () => {
-    const {active, library, chainId} = useWeb3React();
+  const { active, library, chainId } = useWeb3React();
 
-    const platziPunks = useMemo(
-        ()=> {
-            
-            if(active)
-            return new library.eth.Contract( abi, address[chainId]);},
-            //instaciamos  la red, porque si cambiamos la red se  crea otra instancia del contrato
-            
-            [active, chainId, library?.eth?.contract]
-         );
-           
-   return platziPunks
+  const platziPunks = useMemo(
+    () => {
+      if (active) return new library.eth.Contract(abi, address[chainId]);
+    },
+    //instaciamos  la red, porque si cambiamos la red se  crea otra instancia del contrato
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [active, chainId, library?.eth?.contract]
+  );
+
+  return platziPunks;
 };
 
 export default usePlatziPunks;
